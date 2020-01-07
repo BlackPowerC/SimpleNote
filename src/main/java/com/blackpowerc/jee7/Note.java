793c65f8611a63daa8bf14fcc18d8de22d26da40
fatigba72 <fatigba72@gmail.com>
2020-01-07 19:35:42 +0000

    feature(création des notes): ajout de l'entité note.
    Entité annoté avec JPA et BeanValidation.

---
+++ src/main/java/com/blackpowerc/jee7/Note.java
@@ -0,0 +1,103 @@
+package com.blackpowerc.jee7;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "notes")
+public class Note
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id ;
+
+    @Size(max = 127, min = 5, message = "The title of a note must be between 127 and 5 characters")
+    @Column(name = "title", nullable = false, length = 127)
+    private String title ;
+
+    @NotNull
+    @Size(max = 1023, min = 100, message = "The content of a note must be between 1023 and 10 characters")
+    @Column(name = "content", nullable = false, length = 1023)
+    private String content ;
+
+    @Temporal(TemporalType.DATE)
+    private LocalDate publicationDate ;
+
+    public Note(String title, String content, LocalDate publicationDate)
+    {
+        this.title = title;
+        this.content = content;
+        this.publicationDate = publicationDate;
+    }
+
+    public Note() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(id, note.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuffer sb = new StringBuffer("Note{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", content='").append(content).append('\'');
+        sb.append(", publicationDate=").append(publicationDate);
+        sb.append('}');
+        return sb.toString();
+    }
+}
