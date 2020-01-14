f4fc512c100cc14a8bfcd66bbe5b31445e6b709f
fatigba72 <fatigba72@gmail.com>
2020-01-14 21:53:14 +0000

    feature(création des notes): implémentation de NoteService.

--- src/main/java/com/blackpowerc/jee7/NoteService.java
+++ src/main/java/com/blackpowerc/jee7/NoteService.java
@@ -3,6 +3,7 @@ package com.blackpowerc.jee7;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.io.Serializable;
 import java.util.List;
@@ -21,39 +22,42 @@ public class NoteService implements UpdateOperation, ReadOperation
 
     public NoteService() {}
 
-
     @Override
     public List<Note> findAll() {
-        return null;
+        return em.createNamedQuery("note.findAll", Note.class).getResultList() ;
     }
 
     @Override
     public Optional<Note> findById(Serializable id) {
-        return Optional.empty();
+        return Optional.ofNullable(em.find(Note.class, id)) ;
     }
 
     @Override
     public List<Note> find(int limit, int offset) {
-        return null;
+        return em.createNamedQuery("note.findAll", Note.class).setFirstResult(limit*offset).setMaxResults(limit).getResultList() ;
     }
 
     @Override
+    @Transactional
     public int deleteAll() {
-        return 0;
+        return em.createNamedQuery("note.deleteAll").executeUpdate() ;
     }
 
     @Override
+    @Transactional
     public boolean deleteById(Serializable id) {
-        return false;
+        return em.createNamedQuery("note.deleteById").setParameter("id", id).executeUpdate() == 1 ;
     }
 
     @Override
+    @Transactional
     public Note update(@Valid final Note note) {
-        return null;
+        return em.merge(note) ;
     }
 
     @Override
+    @Transactional
     public void insert(@Valid Note note) {
-
+        em.persist(note) ;
     }
 }
